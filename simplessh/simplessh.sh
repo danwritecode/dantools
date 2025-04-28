43e5d2fde7ad3bd6de8ba14ec14903860b673d44
danwritecode <dnelson4993@outlook.com>
2025-04-28 12:24:55 -0400

    upated to use ssh key and optional password

--- simplessh/simplessh.sh
+++ simplessh/simplessh.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
-SERVER_FILE="./servers.txt"
+# Update this accordingly
+SERVER_FILE="/home/dan/bin/dantools/simplessh/servers.txt"
+
+# Update this accordingly
+SSH_KEY="~/.ssh/server_key.pem"
+
+# Check for --password argument
+USE_PASSWORD=0
+if [[ "$1" == "--password" ]]; then
+  USE_PASSWORD=1
+fi
 
 if ! [ -f "$SERVER_FILE" ]; then
   echo "No server list found at $SERVER_FILE"
@@ -19,4 +29,10 @@ fi
 USER_HOST=$(echo "$SELECTED" | awk '{print $2}')
 WINDOW_NAME=$(echo "$SELECTED" | awk '{print $1}')
 
-tmux new-window -n "$WINDOW_NAME" "ssh $USER_HOST || read -p 'Press enter to close...'"
+if [[ $USE_PASSWORD -eq 1 ]]; then
+  # Password-based SSH
+  tmux new-window -n "$WINDOW_NAME" "ssh $USER_HOST || read -p 'Press enter to close...'"
+else
+  # Key-based SSH (default)
+  tmux new-window -n "$WINDOW_NAME" "ssh -i $SSH_KEY $USER_HOST || read -p 'Press enter to close...'"
+fi
